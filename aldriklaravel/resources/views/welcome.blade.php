6c857bd8b89cb6d1de81a4aa08ab9b2e460d526d
AldrikAlvaro <aldrikfariaalvaro@gmail.com>
2022-03-18 16:28:20 -0300

    laravel

--- aldriklaravel/resources/views/welcome.blade.php
+++ aldriklaravel/resources/views/welcome.blade.php
@@ -2,26 +2,40 @@
 @section('title', 'Pagina principal')
 @section('content')    
 
-        <h1>Titulo</h1>
+    <div id="search-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" name="search" id="search" class="form-control" placeholder="Procurar....  ">
+        </form>
+    </div> 
+    
+    
+    <div id="events-container" class="col-md-12">
+        <h2>Proximos Eventos</h2>
+        <P>Veja os eventos dos proximos dias</P>
+        <div id="card-container" class="row">
+            @foreach ($res_evento as $row_evento)
+            <div class="card col-md-3">
+                
+                <img src="{{asset('img/flor.jpg')}}" alt="{{ $row_evento->nome_pokemon}}">
+                <div class="card-body">
+                    <p class="card-date">10/09/2022</p>
+                    <h5 class="card-title">{{ $row_evento->nome_pokemon }}</h5>
+                    <p class="card-participants">x Participantes</p>
+                    <a href="#" class="btn btn-primary">Saber Mais</a>
+                </div>
 
-        @foreach ($res_evento as $row_evento)
-        <p>{{ $row_evento ->nome_pokemon }} -- {{ $row_evento ->tipo }}</p>
-            
-        @endforeach
+            </div>
+            @endforeach
+        </div>
+    </div>      
+
+@endsection
+
+
+
         
-   
-
-        {{-- 
-            //@if($res_nome == "Aldrik")
-                <p>Seja bem vindo Aldrik</p>
-            @else 
-                <p>Erro usuario não encontrado</p>
-            @endif
-
-            @for ($i = 0; $i < count($res_arrey); $i++)
-                    <p>{{ $res_arrey[$i] }} </p>
-            @endfor
-        --}}
-@endsection      
-
-        
+@foreach ($res_evento as $row_evento)
+<p>{{ $row_evento ->nome_pokemon }} -- {{ $row_evento ->tipo }}</p>
+    
+@endforeach
--- aldriklaravel/routes/web.php
+++ aldriklaravel/routes/web.php
@@ -12,13 +12,15 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 use App\Http\Controllers\EventoController;
 
 Route::get('/', [EventoController::class, 'index']);
 Route::get('/eventos/create', [EventoController::class, 'create']);
 
-Route::get('/carros/{id?}', function ($id = null) {
 
+
+Route::get('/carros/{id?}', function ($id = null) {
     $var_busca = request('busca');
     return view('cars', ['res_id' => $id, 'res_busca' => $var_busca]);
 });
